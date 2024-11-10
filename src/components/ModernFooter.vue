a95fd5b94b4d3dfeddc961eaf66521c45d53084e
Tomas Benavides Calderon <tomas.benavidesc@campusucc.edu.co>
2024-11-10 18:12:03 -0500

    Update ModernFooter.vue

--- src/components/ModernFooter.vue
+++ src/components/ModernFooter.vue
@@ -78,8 +78,8 @@
             </h4>
             <ul class="footer-links">
               <li><a href="/"><HomeIcon class="link-icon" /> Home</a></li>
-              <li><a href="/contact"><UserIcon class="link-icon" /> Contact</a></li>
-              <li><a href="/login"><ServicesIcon class="link-icon" /> Dashboar</a></li>
+              <li><a href="/"><UserIcon class="link-icon" /> Contact</a></li>
+              <li><a href="/"><ServicesIcon class="link-icon" /> Dashboard</a></li>
             </ul>
           </div>
           <div class="footer-section">
