81ad3a566cfa8da3b5c2b1d118b9a360f5b35214
T0m4s1n <tomas.benavidesc@campusucc.edu.co>
2024-11-13 21:45:56 -0500

    yes

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,4 +13,3 @@ export default defineConfig({
     outDir: 'dist'
   }
 });
-
